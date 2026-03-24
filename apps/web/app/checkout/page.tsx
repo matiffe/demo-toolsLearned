188a970970726e07bb0288d3a27198946c2a4ac6
Matias Fernandez <matiasfernandez@Matiass-Air.localdomain>
2026-03-24 11:27:31 -0300

    chore: initial turborepo setup

--- apps/web/app/checkout/page.tsx
+++
@@ -1,5 +0,0 @@
-import CheckoutLoader from "./checkout-loader";
-
-export default function CheckoutPage() {
-  return <CheckoutLoader />;
-}
--- packages/ui/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { Button } from "./button";
-import { PromoBanner } from "./promoBanner";
-
-export { PromoBanner, Button };
